18426093948547151b119cbc6d16b503bb0b86cb
RumitEmed <rumit.emed@gmail.com>
2022-04-28 16:29:45 +0530

    Draw circle on finter tap implemented.

--- app/src/main/java/com/etl/rum/rumtestapp/drawRoundOnFingerTouch/DrawRoundOnFingerTouchActivity.java
+++ app/src/main/java/com/etl/rum/rumtestapp/drawRoundOnFingerTouch/DrawRoundOnFingerTouchActivity.java
@@ -1,14 +1,7 @@
 package com.etl.rum.rumtestapp.drawRoundOnFingerTouch;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 
 import com.etl.rum.rumtestapp.R;
 
@@ -18,32 +11,5 @@ public class DrawRoundOnFingerTouchActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_round_on_finter_touch);
-//        setContentView(new DrawingView(this));
-    }
-
-    class DrawingView extends SurfaceView {
-
-        private final SurfaceHolder surfaceHolder;
-        private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        public DrawingView(Context context) {
-            super(context);
-            surfaceHolder = getHolder();
-            paint.setColor(Color.RED);
-            paint.setStyle(Paint.Style.FILL);
-        }
-
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                if (surfaceHolder.getSurface().isValid()) {
-                    Canvas canvas = surfaceHolder.lockCanvas();
-                    canvas.drawColor(Color.BLACK);
-                    canvas.drawCircle(event.getX(), event.getY(), 50, paint);
-                    surfaceHolder.unlockCanvasAndPost(canvas);
-                }
-            }
-            return false;
-        }
     }
 }
