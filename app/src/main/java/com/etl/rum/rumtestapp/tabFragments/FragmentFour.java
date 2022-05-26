884952f48aafa5d5b8944ed40fe3d0591cfe5552
RumitEmed <rumit.emed@gmail.com>
2022-05-26 10:34:39 +0530

    FragmentFour.java changes.

--- app/src/main/java/com/etl/rum/rumtestapp/tabFragments/FragmentFour.java
+++ app/src/main/java/com/etl/rum/rumtestapp/tabFragments/FragmentFour.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.etl.rum.rumtestapp.R;
 
 public class FragmentFour extends Fragment {
