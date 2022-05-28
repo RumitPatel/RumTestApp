4b1a9b8ccdba6aee6241a1f9918e6d0a265759b2
“RumitPatel” <rumit.patel5@gmail.com>
2022-05-28 18:29:21 +0530

    Removed Unused dependencies.

--- app/src/main/java/com/etl/rum/rumtestapp/calenderViewTest/CalenderViewTest.java
+++ app/src/main/java/com/etl/rum/rumtestapp/calenderViewTest/CalenderViewTest.java
@@ -12,13 +12,7 @@ import com.etl.rum.rumtestapp.R;
 
 import java.util.Calendar;
 
-/**
- * Created by FatehAli Asamadi on 12/1/2017.
- */
-
 public class CalenderViewTest extends Activity {
-
-    private CalendarView calendarView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,26 +29,20 @@ public class CalenderViewTest extends Activity {
     }
 
     private void initCalender() {
-        calendarView = findViewById(R.id.calenderview);
+        CalendarView calendarView = findViewById(R.id.calenderview);
 
         Calendar calEndDate = Calendar.getInstance();
         calEndDate.set(Calendar.DAY_OF_MONTH, (calEndDate.get(Calendar.DAY_OF_MONTH) + 7));
 
-//        calendarView.setMinDate(System.currentTimeMillis() - 1000);
-//        calendarView.setMaxDate(calEndDate.getTimeInMillis());
-
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
-//                selectedDateString = year + "-" + (month + 1) + "-" + dayOfMonth;
-//                callScheduleListInfos(selectedDateString);
             }
         });
 
         if ((Build.VERSION.SDK_INT) <= 19) {
             ViewGroup.LayoutParams parameters = calendarView.getLayoutParams();
             parameters.height = 500; //value in pixels
-//            calendarView.setLayoutParams(parameters);
         }
     }
 }
