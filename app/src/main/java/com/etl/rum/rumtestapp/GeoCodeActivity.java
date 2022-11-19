d299b32f071ccc3bb0567a765380af1e3f9ecb52
“RumitPatel” <rumit.patel5@gmail.com>
2022-11-19 08:14:02 +0530

    Get address from lat long without maps API key.

--- app/src/main/java/com/etl/rum/rumtestapp/GeoCodeActivity.java
+++ app/src/main/java/com/etl/rum/rumtestapp/GeoCodeActivity.java
@@ -6,7 +6,9 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.etl.rum.rumtestapp.temp.AppUtils;
 
@@ -38,14 +40,31 @@ public class GeoCodeActivity extends AppCompatActivity {
     }
 
     private void getAddressFromLatLong() {
-        double tempLat = 22.2587;
-        double tempLong = 71.1924;
+        double tempLat = 23.0972046;
+        double tempLong = 72.5404878;
 
         try {
             Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
             List<Address> addresses = geocoder.getFromLocation(tempLat, tempLong, 5);
+            String addressString = "";
             for (int i = 0; i < addresses.size(); i++) {
-                AppUtils.printLogE(addresses.get(i).getPostalCode());
+                String index = (i+1)+"";
+                addressString = !index.isEmpty() ? addressString + "\nindex: " + index + "\n" : addressString;
+                String postalCode = addresses.get(i).getPostalCode();
+                addressString = (postalCode != null && !postalCode.isEmpty()) ? (addressString + ("PostalCode: " + postalCode + "\n")) : addressString;
+                String adminArea = addresses.get(i).getAdminArea();
+                addressString = (adminArea != null && !adminArea.isEmpty()) ? (addressString + ("adminArea: " + adminArea + "\n")) : addressString;
+                String subAdminArea = addresses.get(i).getSubAdminArea();
+                addressString = (subAdminArea != null && !subAdminArea.isEmpty()) ? (addressString + ("subAdminArea: " + subAdminArea + "\n")) : addressString;
+                String locality = addresses.get(i).getLocality();
+                addressString = (locality != null && !locality.isEmpty()) ? (addressString + ("locality: " + locality + "\n")) : addressString;
+                String subLocality = addresses.get(i).getSubLocality();
+                addressString = (subLocality != null && !subLocality.isEmpty()) ? (addressString + ("PostalCode: " + subLocality + "\n")) : addressString;
+            }
+
+            if (!TextUtils.isEmpty(addressString)) {
+                AppUtils.printLogE(addressString);
+                ((TextView)findViewById(R.id.tvAddress)).setText(addressString);
             }
         } catch (IOException e) {
             e.printStackTrace();
